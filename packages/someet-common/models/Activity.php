a676749c1ae607cead551cb341226eb6a0028ce8
maxwelldu <dcj3sjt@163.com>
2016-03-30 16:13:36 +0800

    add profile in activity return fields

--- packages/someet-common/models/Activity.php
+++ packages/someet-common/models/Activity.php
@@ -89,7 +89,9 @@ class Activity extends \yii\db\ActiveRecord
 
     public function extraFields()
     {
-        return ['type', 'user','pma'];
+        return ['type', 'user','pma', 'profile' => function() {
+            return $this->user->profile;
+        }];
     }
 
     /**
